46e53e74aeab743440c00fa737b33f55f1bf97ab
monpeco <monpeco@gmail.com>
2020-06-11 23:48:28 +0000

    Add new word form

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,4 +1,15 @@
 class WordsController < ApplicationController
   def new
   end
+	
+  def create
+    @word = word_params
+    puts @word
+    redirect_to home_index_path
+  end
+
+  private
+    def word_params
+      params.require(:word).permit(:word, :translation, :association)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'words/new'
   get 'home/index'
+  get 'words/new'
+  post 'words/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
